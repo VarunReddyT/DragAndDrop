50f89e001ccef7eaee11b3cde5ae2ae33e62eb4d
Varun-MSI <tvarun2014@gmail.com>
2024-04-18 23:20:14 +0530

    New

---
+++ drag3.gd
@@ -0,0 +1,21 @@
+extends TextureRect
+
+var image_id : int = 0
+
+func _get_drag_data(at_position):
+	var preview_texture = TextureRect.new()
+	
+	preview_texture.texture = texture
+	preview_texture.expand_mode = 1
+	preview_texture.size = Vector2(30, 30)
+	var preview = Control.new()
+	preview.add_child(preview_texture)
+	
+	set_drag_preview(preview)
+
+	image_id = 3
+	var data = {
+		"preview_texture_texture": preview_texture.texture,
+		"image_id": image_id
+	}
+	return data
---
+++ drop3.gd
@@ -0,0 +1,14 @@
+extends TextureRect
+
+func _can_drop_data(at_position, data):
+	# Check if the dropped data is Texture2D
+	if(data["image_id"] == 3):
+		return data["preview_texture_texture"] is Texture2D
+
+func _drop_data(at_position, data):
+	# Set the texture to the dropped data
+	texture = data["preview_texture_texture"]
+	get_node("/root/Main").count += 1
+	if(get_node("/root/Main").count == 4):
+		get_node("/root/Main").count = 0
+		get_tree().change_scene_to_file("res://end.tscn")
